579cbb1100375d2c1260767f3edc504f4aa06e49
MuzakirAhamed <muzakirahmed97@gmail.com>
2023-09-14 16:22:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+let icons = document.getElementById("nav-links");
+let hamburger = document.getElementById("menubar");
+hamburger.addEventListener('click',()=>{
+  icons.classList.toggle('open-menu');
+})
+
+
+
+
+
+
+
